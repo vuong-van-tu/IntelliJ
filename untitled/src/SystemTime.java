fc5da6b4e523810c7a13044244fb471d5c1f477c
vuongvantu <vuongvantu2001x@gmail.com>
2021-05-05 11:46:28 +0700

    'thuchanh'

---
+++ untitled/src/SystemTime.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class SystemTime {
+    public static void main(String[] args){
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
